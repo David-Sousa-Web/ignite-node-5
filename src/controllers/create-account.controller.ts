1f3736cce30bc862282627f12d58d6504b03216c
David Sousa <david01sousa@hotmail.com>
2024-10-17 12:30:19 -0300

    feat: vite config and tsconfig

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -1,20 +1,25 @@
-import { Body, ConflictException, Controller, HttpCode, Post, UsePipes } from "@nestjs/common";
+import {
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from "@nestjs/common";
 import { hash } from "bcryptjs";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
-import { PrismaService } from "src/prisma/prisma.service";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { PrismaService } from "@/prisma/prisma.service";
 import { z } from "zod";
-
 
 const createAccountBodySchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string(),
-})
+});
 
-type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
 
-
-@Controller('/accounts')
+@Controller("/accounts")
 export class CreateAccountController {
   constructor(private prisma: PrismaService) {}
 
@@ -22,28 +27,28 @@ export class CreateAccountController {
   @HttpCode(201)
   @UsePipes(new ZodValidationPipe(createAccountBodySchema))
   async handle(@Body() body: CreateAccountBodySchema) {
-    const {name, email, password} = body
+    const { name, email, password } = body;
 
     const userWithSameEmail = await this.prisma.user.findUnique({
       where: {
         email,
-      }
-    })
-    
-    if ( userWithSameEmail) {
+      },
+    });
+
+    if (userWithSameEmail) {
       throw new ConflictException(
-        'user with same e-mail address already exists'
-      )
+        "user with same e-mail address already exists"
+      );
     }
 
-    const hashedPassword = await hash(password, 8)
+    const hashedPassword = await hash(password, 8);
 
     await this.prisma.user.create({
       data: {
         name,
         email,
         password: hashedPassword,
-      }
-    })
+      },
+    });
   }
 }
